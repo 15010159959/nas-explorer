be53165aadee65a4a0b6e53b7042e6f2337d6e2b
zhechen <15010159959@163.com>
2018-01-31 16:36:37 +0800

    +

--- src/routes/block.vue
+++ src/routes/block.vue
@@ -32,7 +32,7 @@
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#">1556462</a>
+                                    <a href="#">{{ block.height }}</a>
                                 </li>
                                 <li>
                                     <a href="#" aria-label="Next">
@@ -45,50 +45,44 @@
                 </tr>
                 <tr>
                     <td>TimeStamp</td>
-                    <td>2 hrs 3 mins ago (Jan-23-2018 03:53:04 AM +UTC)</td>
+                    <td>{{ block.timestamp }}</td>
                 </tr>
                 <tr>
                     <td>Transactions</td>
                     <td>
-                        <a href="txs.html?block=%Block">274 transactions</a>
+                        <a href="txs.html?block=%Block">{{ block.txCnt }}</a>
                         in this block
                     </td>
                 </tr>
                 <tr>
                     <td>Hash</td>
-                    <td>0x4edab5c4735f10e3a8aa2297848f483858b252560dfbfbe4c9d657d55345e762</td>
+                    <td>{{ block.hash }}</td>
                 </tr>
                 <tr>
                     <td>Parent Hash</td>
                     <td>
-                        <a href="#">0x50780c99ac0b066b95340554c63045970e5b863776a2a0e4ab3e925c0e85d5a6</a>
+                        <a href="block.html?block=%parentHash">{{ block.parentHash }}</a>
                     </td>
                 </tr>
 
                 <tr>
                     <td>Mined By</td>
                     <td>
-                        <a href="address.html?mined">0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5</a>
+                        <a href="address.html?mined">{{ block.miner }}</a>
                         (Nanopool)
                     </td>
                 </tr>
                 <tr>
                     <td>Gas Used</td>
-                    <td>7,984,916 (99.81%)</td>
+                    <td>{{ block.blkGasUsed }} {{ block.blkGasUsedRate }}</td>
                 </tr>
                 <tr>
                     <td>Gas Limit</td>
-                    <td>8,000,029</td>
+                    <td>{{ block.blkGasLimit }}</td>
                 </tr>
                 <tr>
                     <td>Nonce</td>
-                    <td>0x889303e4c001686f3e</td>
-                </tr>
-                <tr>
-                    <td>Block Reward</td>
-                    <td>
-                        3.27657259285203186 Ether (3 + 0.27657259285203186)
-                    </td>
+                    <td>{{ block.nonce }}</td>
                 </tr>
 
             </table>
