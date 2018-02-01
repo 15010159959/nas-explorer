da58a2e800df496e6c492f354ceaf5805023d1f6
zhechen <15010159959@163.com>
2018-02-01 17:47:27 +0800

    +

---
+++ src/routes/oops.vue
@@ -0,0 +1,25 @@
+<style>
+    .container .oops {
+        text-align: center;
+    }
+
+    .image {
+        width: 60%;
+        height: 400px;
+        margin: auto;
+        background: url(../img/oops.jpg) center no-repeat;
+    }
+</style>
+<template>
+    <div class="container">
+        <div class="oops">
+            <h1>Oops!</h1>
+            <h3>The search string you entered was:</h3>
+            <pre></pre>
+            <h4>Sorry! This is an invalid search string.</h4>
+        </div>
+        <div class="image">
+            <div></div>
+        </div>
+    </div>
+</template>
